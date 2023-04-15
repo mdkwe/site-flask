df795b5934cc4d2e0d5bc0dad5c82b45c0c364b3
mdkwe <michelle.dkwe@gmail.com>
2023-04-15 13:03:12 +0200

    mod : affiche render_template

--- fbapp/models.py
+++ fbapp/models.py
@@ -1,15 +1,21 @@
 from flask_sqlalchemy import SQLAlchemy
 import logging as lg
+import enum
 
 from .views import app
 
 # Create database connection object
 db = SQLAlchemy()
 
+class Gender(enum.Enum):
+    female = 0
+    male = 1
+    other = 2
+    
 class Content(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Integer(), nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
 
     def __init__(self, description, gender):
         self.description = description
@@ -18,7 +24,7 @@ class Content(db.Model):
 def init_db():
     db.drop_all()
     db.create_all()
-    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", 1))
-    db.session.add(Content("What's your favorite scary movie?", 0))
+    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", Gender['male']))
+    db.session.add(Content("What's your favorite scary movie?", Gender['female']))
     db.session.commit()
     lg.warning('Database initialized!')
--- fbapp/views.py
+++ fbapp/views.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,7 +9,7 @@ app.config.from_object('config')
 
 @app.route('/')
 def index():
-        return "Hello world !"
+        return render_template('index.html')
 
 if __name__ == "__main__":
         app.run()
